abb70678fa0fdd62152a023cfcd8bfab1109a750
sevenliao <liaoyuwang@126.com>
2018-01-01 22:41:55 +0800

    12

--- h5/2/js/wxShare.js
+++ h5/2/js/wxShare.js
@@ -9,6 +9,7 @@
             url: 'http://www.sevenliao.cn//h5/2/index.html',
             dataType: 'json',
             success: function(data){
+                console.log(data)
                 console.log(encodeURIComponent(location.href),"=====")
                 wx.config({
                    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
